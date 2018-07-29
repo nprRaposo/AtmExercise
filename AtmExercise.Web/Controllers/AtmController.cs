135af22b9dccb7cb30c05f3b81d809fb2d3c1c8f
nprraposo <nikoraposo@gmail.com>
2018-07-29 14:09:37 -0300

    Adding the views of balance and withdrawal

---
+++ AtmExercise.Web/Controllers/AtmController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AtmExercise.Model;
+using AtmExercise.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtmExercise.Web.Controllers
+{
+    public class AtmController : Controller
+    {
+
+        private readonly IService<CreditCard> _ccService;
+        private readonly IService<Operation> _operationService;
+
+        public AtmController(IService<CreditCard> ccService)
+        {
+            this._ccService = ccService;
+        }
+
+        public IActionResult Home(int creditCardId)
+        {
+            return View();
+        }
+
+        public IActionResult Balance()
+        {
+            return PartialView("_Balance");
+        }
+
+        public IActionResult WithDrawal()
+        {
+            return PartialView("_WithDrawal");
+        }
+    }
+}
